a13ea1c8bdcd1ca32dd997b91ba3740b122add15
soffiane <sylar93120@hotmail.fr>
2023-04-04 20:25:34 +0200

    first commit : creation du projet springboot

---
+++ src/main/java/com/transdev/mobiservices/MobiservicesApplication.java
@@ -0,0 +1,13 @@
+package com.transdev.mobiservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobiservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobiservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/transdev/mobiservices/MobiservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transdev.mobiservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobiservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
